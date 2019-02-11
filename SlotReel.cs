69da5fe09cea997e7169832743e292e0dbda455d
22321 <22321@ma-web.nl>
2019-02-11 21:53:36 +0100

    slotwheel script toegevoegd

---
+++ SlotReel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotReel : MonoBehaviour {
+
+    [SerializeField]
+    private int speed = 3;
+    private Transform[] Slots;
+    private Transform currentSlot;
+
+	void Start () {
+        Slots = new Transform[transform.childCount];
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Slots[i] = transform.GetChild(i);
+        }
+    }
+	
+	void Update () {
+        foreach(Transform C in Slots)
+        {
+            // check welk embleem op het midden staat
+            C.transform.Translate(Vector2.down * speed * Time.deltaTime);
+            if (C.position.y < this.transform.position.y)
+            {
+                currentSlot = C.transform;
+                print(currentSlot.name);
+            }
+            if(C.position.y < this.transform.position.y -100) //verplaats naar top
+            {
+                C.transform.position = new Vector2(transform.position.x, transform.position.y + 175);
+            }
+        }
+	}
+}
